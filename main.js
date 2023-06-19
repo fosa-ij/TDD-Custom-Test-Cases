2258183a98c85ff50adb28afeda61e9a1861269d
fosa-ij <efosaij@gmail.com>
2023-06-19 23:10:16 +0100

    update-test-cases

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-export function add(x, y) {
-    return x + y;
+
+export const makeUpperCase = string => {
+    return typeof string !== 'number' ? string.toUpperCase() : 'Numbers are not an acceptable argument'
 }
+
+export const reverseWords = string => {
+
+    return string.split(' ').reverse().join(' ');
+}
+console.log(reverseWords('hello world'));
+
+// export const removeVowels = string => {
+//     let vowel = 'aeiou';
+//     const arr = string.split('')
+//     arr.forEach((item, index, array) => vowel.includes(item.toLowerCase()) ? array.splice(index, 1) : item)
+//     return arr.join('')
+// }
+// console.log(removeVowels('efOsaisU'));
--- main.test.js
+++ main.test.js
@@ -1,20 +1,70 @@
 import { expect, assert } from 'chai'
 // const assert = require('assert')
-import { add } from './main.js'
+import { makeUpperCase, reverseWords } from './main.js'
 
-describe('Application can perform addition', () => {
-    it('should add numbers', () => {
-        // assert.equal(2 + 5, 7, "Addition wrong")
-        // expect(add(2, 5)).to.equal(7);
-        assert.strictEqual(add(2, 8), 10, "Wrong Result")
+
+describe("Convert to UPPERCASE", () => {
+    it("should convert all strings to upppercase", () => {
+        assert.strictEqual(makeUpperCase(""), "");
+        assert.strictEqual(makeUpperCase("hello"), "HELLO");
+        assert.strictEqual(makeUpperCase("Hello"), "HELLO");
+        assert.strictEqual(makeUpperCase("HELLO"), "HELLO");
+    });
+
+    it("should not accept number parameters", () => {
+        assert.equal(makeUpperCase(8), 'Numbers are not an acceptable argument')
     })
-    // it('should only add numbers', () => {
-    //     expect(add('5', 10)).to.equal('Parameter is not a number')
-    // })
-})
 
+    it("should not accept more than one parameter", () => {
+        assert.strictEqual(makeUpperCase('name', 'name2'), 'NAME', 'This function executes only the first argument')
+    })
+});
+
+describe("reverseWords", () => {
+    it("should reverse the order of the string parameter", () => {
+      assert.strictEqual(reverseWords("hello world!"), "world! hello")
+      assert.strictEqual(reverseWords("fosa doesn't speak like this"), "this like speak doesn't fosa")
+      assert.strictEqual(reverseWords("foobar"), "foobar")
+      assert.strictEqual(reverseWords("kata editor"), "editor kata")
+      assert.strictEqual(reverseWords("row row row your boat"), "boat your row row row")
+      assert.strictEqual(reverseWords(""), "")
+    });
+
+    it("should not accept number parameters", () => {
+        assert.equal(makeUpperCase(9), 'Numbers are not an acceptable argument')
+    })
+  });
+
+//   describe("removeVowels", () => {
+//     it("should remove each vowel in the string parameter", () => {
+//       assert.strictEqual(removeVowels("hello world!"), "hll wrld!")
+//       assert.strictEqual(removeVowels("foobar"), "fbr")
+//       assert.strictEqual(removeVowels("editor"), "dtr")
+//       assert.strictEqual(removeVowels("vowels"), "vwls")
+//       assert.strictEqual(removeVowels("i"), "")
+//     });
+
+//     it("should not accept number parameters", () => {
+//         assert.equal(makeUpperCase(9), 'Numbers are not an acceptable argument')
+//     })
+
+//     it("should handle uppercase strings", () => {
+//         assert.equal(makeUpperCase('Efosa'), 'fs')
+//     })
+//   });
+
+// describe('Application can perform addition', () => {
+//     it('should add numbers', () => {
+//         // assert.equal(2 + 5, 7, "Addition wrong")
+//         // expect(add(2, 5)).to.equal(7);
+//         assert.strictEqual(add(2, 8), 10, "Wrong Result")
+//     })
+//     // it('should only add numbers', () => {
+//     //     expect(add('5', 10)).to.equal('Parameter is not a number')
+//     // })
+// })
 // describe('Application can perform MULTIPLICATION', () => {
-//     it('should multiply numbers', () => {
+    //     it('should multiply numbers', () => {
 //         // expect(multiply(5, 10).to.equal(50))
 //         assert.strictEqual(multiply(5, 20), 100, 'Check that parameters are numbers')
 //     })
